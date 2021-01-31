79c29152983d321a43924f6d500efc533405938b
MurodTuraev <tml.murod@mail.ru>
2021-01-31 16:55:49 +0500

    Get updates

---
+++ getUpdates.py
@@ -0,0 +1,8 @@
+import requests
+from pprint import pprint
+token = '1597735413:AAEA20RoZ_JJsQ3b4q8XvKuegECeH-i6exk'
+url = f'https://api.telegram.org/bot{token}/getUpdates'
+r = requests.get(url)
+data = r.json()
+updates = data['result']
+pprint(updates)
