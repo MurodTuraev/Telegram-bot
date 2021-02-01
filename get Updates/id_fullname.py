813a0de4f0b47893e47ee21a848f0e004e7cbc44
Murod Turaev (24.01.2021) <tml.murod@mail.ru>
2021-02-01 11:48:07 +0500

    id plus fullname

---
+++ get Updates/id_fullname.py
@@ -0,0 +1,15 @@
+import requests
+from pprint import pprint
+token = '1597735413:AAEA20RoZ_JJsQ3b4q8XvKuegECeH-i6exk'
+url = f'https://api.telegram.org/bot{token}/getUpdates'
+r = requests.get(url)
+data = r.json()
+updates = data['result']
+
+for update in updates:
+    message = update['message']
+    first = message['from'].get('first_name', '')
+    last = message['from'].get('last_name', '')
+    id = message['from'].get('id', '')
+    fullname = first+' '+last
+    print(f'{id} {fullname}')
