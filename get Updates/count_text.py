0a53a462d60bfca34b76568fcc53bbd103c329de
Murod Turaev (24.01.2021) <tml.murod@mail.ru>
2021-02-01 11:58:07 +0500

    name plus text

---
+++ get Updates/count_text.py
@@ -0,0 +1,13 @@
+import requests
+from pprint import pprint
+token = '1597735413:AAEA20RoZ_JJsQ3b4q8XvKuegECeH-i6exk'
+url = f'https://api.telegram.org/bot{token}/getUpdates'
+r = requests.get(url)
+data = r.json()
+updates = data['result']
+
+for update in updates:
+    message = update['message']
+    text = message['text']
+    first = message['from'].get('first_name')
+    print(f'{first} :  {text}')
