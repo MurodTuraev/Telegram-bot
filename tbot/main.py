f3689c9b8029e45a8e67d00a8e5fc6be1651d57e
Murod Turaev (24.01.2021) <tml.murod@mail.ru>
2021-02-02 13:39:33 +0500

    echo

---
+++ tbot/main.py
@@ -0,0 +1,16 @@
+import telebot
+
+bot = telebot.TeleBot('1597735413:AAEA20RoZ_JJsQ3b4q8XvKuegECeH-i6exk')
+
+
+@bot.message_handler()
+def echo_messages(*messages):
+    """
+    Echoes all incoming messages of content_type 'text'.
+    """
+    for m in messages:
+        chatid = m.chat.id
+        if m.content_type == 'text':
+            text = 'uzing ' + m.text
+            bot.send_message(chatid, text)
+bot.polling()
