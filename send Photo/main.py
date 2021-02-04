4c315cab2f53fecd5bdc30de93385c0e54b18660
Murod Turaev (24.01.2021) <tml.murod@mail.ru>
2021-02-04 13:40:50 +0500

    send Photo with URL

--- send Photo/main.py
+++ send Photo/main.py
@@ -7,6 +7,6 @@ url = f'https://api.telegram.org/bot{token}/sendPhoto'
 
 paramet = {
     'chat_id': 913536871,
-    'photo': 'AgACAgIAAxkBAAN4YBunv2JzPeug83nWvfUxLPW_u0MAAo-yMRv-euBIRPfXkgvgjnTMF6KWLgADAQADAgADeQADswQHAAEeBA'
+    'photo': 'https://storage.ws.pho.to/s2/f607aa4c6a291a39cc8c654481fe72568435da6c_m.jpg'
 }
 r = requests.get(url, params=paramet)
